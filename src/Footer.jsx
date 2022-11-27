9b04a0d05f10f27d62ffcca0fae533d611fddb88
bsmuse <bsmuse@tutamail.com>
2022-11-26 19:20:21 -0700

    Fixed images

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,10 +4,10 @@ import './Footer.css'
 function Footer() {
  return (
     <div className="footer">
-        <a href='https://twitter.com/BSMinstrel'><img className="icon" src='src/assets/twitter_icon.png' alt='twitter icon'></img></a>
-        <img className="icon"src='src/assets/facebook_icon.png' alt='facebook icon'></img>
-        <img className="icon" src='src/assets/instagram_icon.png' alt='instagram icon'></img>
-        <a href='https://github.com/BSMuse'><img className="icon" src='src/assets/github_icon.png' alt='github icon'></img></a>
+        <a href='https://twitter.com/BSMinstrel'><img className="icon" src='assets/twitter_icon.png' alt='twitter icon'></img></a>
+        <img className="icon"src='assets/facebook_icon.png' alt='facebook icon'></img>
+        <img className="icon" src='assets/instagram_icon.png' alt='instagram icon'></img>
+        <a href='https://github.com/BSMuse'><img className="icon" src='assets/github_icon.png' alt='github icon'></img></a>
     </div>
   )
 }
--- src/Intro.jsx
+++ src/Intro.jsx
@@ -4,17 +4,17 @@ import './Intro.css'
 function Intro() {
   return (
     <div className='intro'>
-      <img src='src/assets/headshot_2.png' alt='head shot of astonishingly handsome man'></img>
+      <img src='assets/headshot_2.png' alt='head shot of astonishingly handsome man'></img>
       <h1>Alexander Da Costa</h1>
       <h3>Web Developer</h3>
       <p className="website">alexanderdacosta.dev</p>
       <div className='buttons'>
         <a className="email" href='mailto:alexander.a.dacosta@gmail.com'>
-          <img src="src/assets/mail.png" alt="linkedin logo" />  
+          <img src="assets/mail.png" alt="linkedin logo" />  
           <p>Email</p>
         </a>
         <a className="linkedin" href='https://www.linkedin.com/in/alexander-dacosta/'>
-        <img src="src/assets/linkedin.png" alt="linkedin logo" />  
+        <img src="assets/linkedin.png" alt="linkedin logo" />  
           <p>Linkedin</p>
         </a>
       </div>
